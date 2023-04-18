3a6b96be2f776e5d03d2cea90a48393312b8a1c6
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2023-04-18 11:37:11 +0200

    Update parser to work on GraphQL results as HTML no longer contains the data rendered.

--- src/libs/server/parsers/dev-to.parser.ts
+++ src/libs/server/parsers/dev-to.parser.ts
@@ -1,22 +1,22 @@
-import {parse} from "node-html-parser";
-import {format} from "date-fns";
+import { format } from 'date-fns';
+import { parse } from 'node-html-parser';
 
 export class DevtoParser {
-    static getPosts(html: string) {
-        const root = parse(html);
-        const articles = root.querySelectorAll(".crayons-story").slice(0, 3);
+	static getPosts(html: string) {
+		const root = parse(html);
+		const articles = root.querySelectorAll('.crayons-story').slice(0, 3);
 
-        return articles.map((article) => {
-            const linkEl = article.querySelector("h2 a")!;
+		return articles.map((article) => {
+			const linkEl = article.querySelector('h2 a')!;
 
-            const title = linkEl.text.trim();
-            const link = 'https://dev.to' + linkEl.getAttribute("href");
-            const date = format(
-                new Date(article.querySelector("time")!.getAttribute("datetime")!),
-                "MMM d, yyyy"
-            );
+			const title = linkEl.text.trim();
+			const link = 'https://dev.to' + linkEl.getAttribute('href');
+			const date = format(
+				new Date(article.querySelector('time')!.getAttribute('datetime')!),
+				'MMM d, yyyy'
+			);
 
-            return {title, description: "", date, link};
-        });
-    }
+			return { title, description: '', date, link };
+		});
+	}
 }
--- src/libs/server/parsers/medium.parser.ts
+++ src/libs/server/parsers/medium.parser.ts
@@ -1,38 +1,31 @@
-import {parse} from "node-html-parser";
+import { format } from 'date-fns';
+import { parse } from 'node-html-parser';
 
 export class MediumParser {
-    static getPosts(html: string, username: string) {
-        const root = parse(html);
-        const articles = root.querySelectorAll("article").slice(0, 3);
+	static getPosts(html: string, username: string) {
+		const root = parse(html);
+		const scripts = root.querySelectorAll('script');
 
-        return articles.map((article) => {
-            const links = article.querySelectorAll("a")!;
-            // Title idx is (kind of) the most reliable anchor to find data and description around
-            const titleIdx = links.findIndex((link) => link.querySelector("h2"))!;
+		const preloaded = scripts.find((script) =>
+			script.innerText.includes('window.__APOLLO_STATE__')
+		);
+		const preloadedData = JSON.parse(
+			preloaded!.innerText.replace('window.__APOLLO_STATE__ = ', '')
+		);
 
-            // Sometimes there are two spans, first one containing a separator
-            // Recently published articles don't have span, so we query for p
-            let date = links[titleIdx - 1].querySelectorAll("span").at(-1)?.text ?? links[titleIdx - 1].querySelector("p")!.text;
-            const title = links[titleIdx].querySelector("h2")!.text;
-            // Sometimes title and descriptions share the a tag
-            const description =
-                links[titleIdx].querySelector("p")?.text ||
-                links[titleIdx + 1].querySelector("p")!.text;
-
-            // Medium is not adding Year for this year's articles. So we add it manually
-            if (/[A-Za-z]{3}\s\d{1,2}$/.test(date)) {
-                date += `, ${new Date().getFullYear()}`
-            }
-
-            const postLink = links[titleIdx].getAttribute("href")!.split("?")[0];
-            // If there is only one "/" then the link should have the username
-            // otherwise it already has correct path
-            // The workaround is needed because profiles with branded pages don't have
-            // prefixes in posts URLs and hence invalid link would be created otherwise
-            const prefix = postLink.match(/\//g)!.length === 1 ? `/@${username}` : "";
-            const link = "https://medium.com" + prefix + postLink;
-
-            return {title, description, date, link};
-        });
-    }
+		return (
+			Object.entries(preloadedData)
+				.filter(([key]) => key.startsWith('Post'))
+				.slice(0, 3)
+				// Results are in a form of ["Post:...", { ... post data }]
+				.map(([_, post]: [any, any]) => {
+					return {
+						title: post.title,
+						description: '',
+						date: format(new Date(post.firstPublishedAt), 'MMM d, yyyy'),
+						link: post.mediumUrl
+					};
+				})
+		);
+	}
 }
